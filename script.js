440a44cacf6e46a4ad8b9165a88db1dfb4fb50b9
soni shivam7544 <sonishivam7544@gmail.com>
2024-11-16 22:52:07 +0530

    aadhar asked

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 console.log('Hello this is my first javascript print statement');
 age=prompt("Enter your age");
 console.log(`you are ${age} years old`);
-console.log ("You are eligible to vote");
+console.log ("Do you have aadhar?");
