83cd2b44fcd218ded893b42bfb09bca68bb607d5
soni shivam7544 <sonishivam7544@gmail.com>
2024-11-17 00:29:26 +0530

    age changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log('Hello this is my first javascript print statement');
 age=prompt("Enter your age");
-console.log(`you are ${age} years old`);
+console.log(`you are ${age +1} years old`);
 console.log ("Do you have aadhar?");
 console.log("greetings");
 console.log("closures");
